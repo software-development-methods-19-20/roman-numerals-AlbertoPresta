28e86cc39dd7acb6edda77bfcbef94ac3a76bebc
apresta <presta.alberto@libero.it>
2019-11-12 09:32:04 +0100

    first commit

--- test/source/test/roman/DecimalToRomanNumeralTest.java
+++ test/source/test/roman/DecimalToRomanNumeralTest.java
@@ -1,5 +1,15 @@
 package test.roman;
 
+import dssc.roman.Romannumeral;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertion.assertEquals;
 public class DecimalToRomanNumeralTest {
+
+
+    @Test
+    void zeroIsEmptyString(){
+        RomanNumeral romanNumeral = new RomanNumeral(0);
+        assertEquals("",romanNumeral.toString());
+    }
 
 }
